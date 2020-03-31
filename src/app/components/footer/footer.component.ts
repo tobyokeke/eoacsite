23427a670624a2ab7614db1ae9a892077089ec0e
Toby Okeke <toby.okeke@gmail.com>
2020-03-31 16:06:12 +0100

    chore: content udpates

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../services/data.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  services;
+  constructor(
+    public dataService : DataService
+  ) { }
 
   ngOnInit(): void {
+    this.services = this.dataService.services;
   }
 
 }
